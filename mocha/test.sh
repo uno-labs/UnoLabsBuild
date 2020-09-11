19f81eab854b4adc2d65be51ed9d85ee54d5e760
Igor Bochkariov <ujifgc@gmail.com>
2020-09-11 12:57:50 +0400

    add mockup package info

--- mocha/test.sh
+++ mocha/test.sh
@@ -3,7 +3,6 @@
 ROOT="$(dirname $(readlink -f $0))"
 
 npm install --save-dev mocha
-npm init -y
 ln -sf $ROOT/../build/bin/Release_Browser_wasm32/UnoSemuxLightCoreWasm.js ./
 ln -sf $ROOT/../build/bin/Release_Browser_wasm32/UnoSemuxLightCoreWasm.wasm ./
 npm test
